c7b9e7ccd9ac594f5bcff22fbf6bbd7ba2e776f6
Aleksandr Kochagov <kochagov@lecta.ru>
2019-07-23 18:04:48 +0300

    initial commit

---
+++ DinosaurusPark/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DinosaurusPark.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
