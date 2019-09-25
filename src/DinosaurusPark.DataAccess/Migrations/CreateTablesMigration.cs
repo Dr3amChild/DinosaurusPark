bbd29b91777426a6827e9570fe78dbaa5cf88472
Aleksandr Kochagov <kochagov@lecta.ru>
2019-09-25 15:25:48 +0300

    added deleting tables in migration

--- src/DinosaurusPark.DataAccess/Migrations/CreateTablesMigration.cs
+++ src/DinosaurusPark.DataAccess/Migrations/CreateTablesMigration.cs
@@ -6,26 +6,28 @@ namespace DinosaurusPark.DataAccess.Migrations
     [Migration(1)]
     public class CreateTablesMigration : Migration
     {
+        private const string SpeciesTable = "Species";
+        private const string DinosaurusTable = "Dinosaurs";
+
         public override void Up()
         {
-            const string speciesTable = "Species";
             const string idColumn = "Id";
-
-            Create.Table(speciesTable)
+            Create.Table(SpeciesTable)
                 .WithColumn(idColumn).AsInt32().PrimaryKey("PK_Species").NotNullable()
                 .WithColumn("Name").AsString().NotNullable()
                 .WithColumn("Description").AsString().NotNullable()
                 .WithColumn("FoodType").AsInt32().NotNullable();
 
-            Create.Table("Dinosaurs")
+            Create.Table(DinosaurusTable)
                 .WithColumn(idColumn).AsInt32().PrimaryKey("PK_Dinosaurs").NotNullable()
-                .WithColumn("SpeciesId").AsInt32().ForeignKey("FK_SpeciesId", speciesTable, idColumn).NotNullable()
+                .WithColumn("SpeciesId").AsInt32().ForeignKey("FK_SpeciesId", SpeciesTable, idColumn).NotNullable()
                 .WithColumn("Name").AsString().NotNullable();
         }
 
         public override void Down()
         {
-            throw new NotImplementedException();
+            Delete.Table(DinosaurusTable);
+            Delete.Table(SpeciesTable);
         }
     }
 }
