ace72a5e76f1f56549c164eb9db9c20d408ab2ff
DreamChild <DreamChild@DESKTOP-FJG1T7S>
2019-10-16 22:20:30 +0300

    tests for data retrieving

---
+++ tests/DinosaurusPark.IntegrationTests/Responses/DinosaursResponse.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace DinosaurusPark.IntegrationTests.Responses
+{
+    public class DinosaursResponse
+    {
+        public IReadOnlyCollection<DinosaurResponse> Items { get; set; }
+
+        public class DinosaurResponse
+        {
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+
+            public int Gender { get; set; }
+
+            public int SpeciesId { get; set; }
+        }
+    }
+}
--- tests/DinosaurusPark.IntegrationTests/Tests/DinosaurusControllerTests.cs
+++ tests/DinosaurusPark.IntegrationTests/Tests/DinosaurusControllerTests.cs
@@ -1,4 +1,6 @@
 using DinosaurusPark.IntegrationTests.Apis;
+using DinosaurusPark.IntegrationTests.Requests;
+using DinosaurusPark.IntegrationTests.Responses;
 using DinosaurusPark.WebApplication.Validation;
 using NUnit.Framework;
 using System;
@@ -9,25 +11,59 @@ namespace DinosaurusPark.IntegrationTests.Tests
 {
     public class DinosaurusControllerTests : BaseTests
     {
-        private IDinosaurusControllerApi _api;
+        private IDinosaurusControllerApi _dinosaursApi;
+        private IGenerationControllerApi _generatorApi;
 
         [SetUp]
         public void Setup()
         {
-            _api = GetApi<IDinosaurusControllerApi>();
+            _dinosaursApi = GetApi<IDinosaurusControllerApi>();
+            _generatorApi = GetApi<IGenerationControllerApi>();
         }
 
         [Test]
         public async Task GetAll_ReturnsOk_If_RequestIsCorrect()
         {
-            var result = await _api.GetAll<string>(1, 10);
+            var result = await _dinosaursApi.GetAll<string>(1, 10);
             Assert.AreEqual(result.StatusCode, HttpStatusCode.OK);
+        }
+
+        [Test]
+        public async Task GetAll_ReturnsFullPage_IfRequiredItemsWasGenerated()
+        {
+            const int expectedCount = 5;
+            var generationRequest = new GenerationRequest
+            {
+                SpeciesCount = 1,
+                DinosaursCount = expectedCount,
+            };
+
+            await _generatorApi.Generate<GenerationResponse>(generationRequest);
+            var result = await _dinosaursApi.GetAll<DinosaursResponse>(expectedCount, 0);
+            Assert.AreEqual(result.Content.Items.Count, expectedCount);
+        }
+
+        [Test]
+        [Ignore("Need [TearDown] logic")]
+        public async Task GetAll_ReturnsLessThanExpectedCount_IfThereWasGeneratedLessThanRequired()
+        {
+            const int actualCount = 4;
+            var generationRequest = new GenerationRequest
+            {
+                SpeciesCount = 1,
+                DinosaursCount = actualCount,
+            };
+
+            const int expectedCount = 5;
+            await _generatorApi.Generate<GenerationResponse>(generationRequest);
+            var result = await _dinosaursApi.GetAll<DinosaursResponse>(expectedCount, 0);
+            Assert.AreEqual(result.Content.Items.Count, actualCount);
         }
 
         [Test]
         public async Task GetAll_ReturnsBadRequest_If_CountIsNotPositive()
         {
-            var result = await _api.GetAll<string>(-1, 10);
+            var result = await _dinosaursApi.GetAll<string>(-1, 10);
             Assert.AreEqual(result.StatusCode, HttpStatusCode.BadRequest);
             Assert.GreaterOrEqual(result.Error.Content.IndexOf(ErrorCodes.CountIsNegativeOrZero, StringComparison.CurrentCulture), 0);
         }
@@ -35,7 +71,7 @@ namespace DinosaurusPark.IntegrationTests.Tests
         [Test]
         public async Task GetAll_ReturnsBadRequest_If_OffsetIsNegative()
         {
-            var result = await _api.GetAll<string>(1, -1);
+            var result = await _dinosaursApi.GetAll<string>(1, -1);
             Assert.AreEqual(result.StatusCode, HttpStatusCode.BadRequest);
             Assert.GreaterOrEqual(result.Error.Content.IndexOf(ErrorCodes.OffsetIsNegative, StringComparison.CurrentCulture), 0);
         }
