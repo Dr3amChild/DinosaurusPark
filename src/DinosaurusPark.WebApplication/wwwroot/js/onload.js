410675495eba62a8c0021d8f932cfbb281ab7f70
DreamChild <DreamChild@DESKTOP-FJG1T7S>
2019-10-23 21:46:00 +0300

    generation improvements

--- src/DinosaurusPark.WebApplication/wwwroot/js/onload.js
+++ src/DinosaurusPark.WebApplication/wwwroot/js/onload.js
@@ -1,7 +1,10 @@
 ﻿window.onload = async () => {
     document.getElementById("generate-btn").addEventListener("click", async () => {
-        window.generate(10, 100);
+        const speciesCount = document.getElementById("species-count-text").value;
+        const dinosaursCount = document.getElementById("dinosaurs-count-text").value;
+        await window.generate(speciesCount, dinosaursCount);
         const result = await window.loadDinosaurs(1, 10);
+        document.getElementById("no-data-alert").style.display = "none";
         window.show(result);
         setPaging(result, onPageClick);
     });
