9e29640be89bdb7efa56a837ad5314148163b7e2
Aleksandr Kochagov <kochagov@lecta.ru>
2019-10-25 13:00:25 +0300

    a little fix

--- src/DinosaurusPark.WebApplication/wwwroot/js/modal/modal.jsx
+++ src/DinosaurusPark.WebApplication/wwwroot/js/modal/modal.jsx
@@ -4,7 +4,7 @@
             <div className="modal-dialog modal-dialog-centered" role="document">
                 <div className="modal-content">
                     <div className="modal-header">
-                        <h5 className="modal-title" id="dinosaur-modal-title">{this.props.modalHeader}</h5>
+                        <h5 className="modal-title">{this.props.modalHeader}</h5>
                         <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
