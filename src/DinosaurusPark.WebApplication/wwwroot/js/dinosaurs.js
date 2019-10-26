6e99df3d2225098481162d4d5bdb995467d17a0d
DreamChild <DreamChild@DESKTOP-FJG1T7S>
2019-10-26 14:22:53 +0300

    moved paging into jsx-file

--- src/DinosaurusPark.WebApplication/wwwroot/js/dinosaurs.js
+++ src/DinosaurusPark.WebApplication/wwwroot/js/dinosaurs.js
@@ -7,17 +7,16 @@ function show(paging) {
     ReactDOM.render(React.createElement(Cards, { items: paging.items }), area);
 }
 
-function setPaging(paging, onClick) {
-    const pagingList = document.getElementById("paging-list");
-    pagingList.innerHTML = "";
-    for (let pageNum = 1; pageNum <= paging.pagesCount; pageNum++) {
-        const page = document.createElement("li");
-        page.className = pageNum === paging.pageNumber ? "page-item active" : "page-item";
-        page.setAttribute("page-num", pageNum);
-        page.addEventListener("click", onClick);
-        page.innerHTML = `<span class='page-link'>${pageNum}</span>`;
-        pagingList.appendChild(page);
-    }
+function setPaging(paging, onClick) {    
+    const pagingArea = document.getElementById("paging-nav");
+    ReactDOM.unmountComponentAtNode(pagingArea);
+    ReactDOM.render(React.createElement(
+        Paging,
+        {
+            pagesCount: paging.pagesCount,
+            activePage: paging.pageNumber,
+            onClick
+        }), pagingArea);
 }
 
 async function onPageClick(e, api) {
---
+++ src/DinosaurusPark.WebApplication/wwwroot/js/paging.jsx
@@ -0,0 +1,27 @@
+﻿class Paging extends React.Component {
+    render() {
+        return (
+            <ul id="paging-list" className="pagination pagination-lg">
+                {this.createPages()}
+            </ul>
+        );
+    }
+
+    createPages() {
+        const pages = [];        
+        for (let idx = 1; idx <= this.props.pagesCount; ++idx) {
+            pages.push(<Page key={idx} pageNum={idx} activePage={this.props.activePage} onClick={this.props.onClick} />);
+        }
+        return pages;
+    }
+}
+
+class Page extends React.Component {
+    render() {
+        return (
+            <li className={this.props.pageNum === this.props.activePage ? "page-item active" : "page-item"} onClick={this.props.onClick} page-num={this.props.pageNum} >
+                <span className='page-link'>{this.props.pageNum}</span>
+            </li>
+        );
+    }
+}
