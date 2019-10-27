4b64419182909cd66b024e73eed2589f30635daa
DreamChild <DreamChild@DESKTOP-FJG1T7S>
2019-10-27 17:57:13 +0300

    validation

--- src/DinosaurusPark.WebApplication/wwwroot/js/generation.jsx
+++ src/DinosaurusPark.WebApplication/wwwroot/js/generation.jsx
@@ -1,11 +1,24 @@
 ﻿class GenerationForm extends React.Component {
+    constructor() {
+        this.validator = new FormValidator("validation-error");
+    }
+
     render() {
         const items = [];
         items.push(<h5 className="card-title" key="1">Динозавров пока нет. Но вы можете их сгенерировать.</h5>);
         items.push(<FormGroup key="2" id="species-count-text" title="Количество видов:" text={this.props.species} placeholder="Введите значение" />);
         items.push(<FormGroup key="3" id="dinosaurs-count-text" title="Количество динозавров:" text={this.props.dinosaurs} placeholder="Введите значение" />);
-        items.push(<button type="button" key="4" className="btn btn-info" onClick={this.props.onClick}>Сгенерировать</button>);
+        items.push(<button type="button" key="4" className="btn btn-info" onClick={this.onGenerateClick}>Сгенерировать</button>);
         return items;
+    }
+
+    onGenerateClick() {
+        const inputs = [];
+        inputs.push(document.getElementById("species-count-text"));
+        inputs.push(document.getElementById("dinosaurs-count-text"));
+        if (this.validator.validateInputs(inputs)) {
+            this.props.onClick();
+        }
     }
 }
 
---
+++ src/DinosaurusPark.WebApplication/wwwroot/js/validator.js
@@ -0,0 +1,28 @@
+﻿class FormValidator {
+    constructor(errorClass) {
+        this.errorClass = errorClass;
+    }
+
+    validateInputs(inputs) {
+        let result = true;
+        for (let input of inputs) {
+            const isValid = this.validateNumber(input.value, input.getAttribute("min"), input.getAttribute("max"));
+            if (isValid) {
+                input.classList.remove(this.errorClass);
+            } else {
+                input.classList.push(this.errorClass);
+                result = false;
+            }
+        }
+        return result;
+    }
+
+    validateNumber(value, min, max) {
+        const number = Math.floor(Number(value));
+        return number !== Infinity &&
+               String(number) === value &&
+               number > 0 &&
+               number >= min &&
+               number <= max;
+    }
+}
