d5597e7c7f4531f3f8ac8d1fb2869c94a11c38ca
Aleksandr Kochagov <kochagov@lecta.ru>
2019-12-20 11:05:43 +0300

    added tests for FirstUp method of StringExtensions

---
+++ tests/DinosaurusPark.UnitTests/StringExtensionsTest.cs
@@ -0,0 +1,49 @@
+using System;
+using DinosaurusPark.Extensions;
+using NUnit.Framework;
+
+namespace DinosaurusPark.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void FrstUpExtensionMethod_ShouldThrow_If_InputStringIsNull()
+        {
+            static void FirstUpAction()
+            {
+                ((string) null).FirstUp();
+            } 
+
+            Assert.Throws<ArgumentNullException>(FirstUpAction);
+        }
+
+
+        [Test]
+        public void FrstUpExtensionMethod_ShouldThrow_If_InputStringIsEmpty()
+        {
+            static void FirstUpAction()
+            {
+                "".FirstUp();
+            }
+
+            Assert.Throws<ArgumentException>(FirstUpAction);
+        }
+
+        [Test]
+        [TestCase("name", "Name")]
+        [TestCase("Name", "Name")]
+        [TestCase(" name", " name")]
+        [TestCase("1 name", "1 name")]
+        [TestCase("NAME", "Name")]
+        public void FrstUpExtensionMethod_Should_If_InputStringIsEmpty(string input, string expected)
+        {
+            string result = input.FirstUp();
+           Assert.AreEqual(result, expected);
+        }
+    }
+}
