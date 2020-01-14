0cfe86a5d201c811e36a1edde1a6957b64f8b6f4
Aleksandr Kochagov <kochagov@lecta.ru>
2020-01-14 15:17:50 +0300

    new integration testst for information controller

---
+++ tests/DinosaursPark.IntegrationTests/Responses/ParkInformationResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace DinosaursPark.WebApplication.Responses
+{
+    public class ParkInformationResponse
+    {
+        public string Name { get; set; }
+
+        public double Area { get; set; }
+
+        public string Address { get; set; }
+
+        public int DinosaursCount { get; set; }
+
+        public int SpeciesCount { get; set; }
+    }
+}
