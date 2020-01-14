3302e93b00bd7d222704e2dfa4d539b24d45e4bf
Aleksandr Kochagov <kochagov@lecta.ru>
2020-01-14 11:01:42 +0300

    added information controller api

---
+++ tests/DinosaursPark.IntegrationTests/Apis/IInformationControllerApi.cs
@@ -0,0 +1,16 @@
+﻿using DinosaursPark.IntegrationTests.Requests;
+using Refit;
+using System.Threading.Tasks;
+
+namespace DinosaursPark.IntegrationTests.Apis
+{
+    [Headers("Content-Type: application/json")]
+    internal interface IInformationControllerApi
+    {
+        [Get("/information/park")]
+        Task<ApiResponse<T>> GetParkInfo<T>();
+
+        [Get("/information/species")]
+        Task<ApiResponse<T>> GetSpeciesInfo<T>();
+    }
+}
