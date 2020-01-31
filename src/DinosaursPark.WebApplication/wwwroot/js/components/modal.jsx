00a45ceff29a79d52866c0fa8e47857ebe2fccee
DreamChild <dreamchildmail@gmail.com>
2020-01-31 22:20:52 +0300

    frontend fixes

--- src/DinosaursPark.WebApplication/wwwroot/js/components/modal.jsx
+++ src/DinosaursPark.WebApplication/wwwroot/js/components/modal.jsx
@@ -58,7 +58,7 @@ class Footer extends React.Component {
     render() {
         return (
             <div className="modal-footer">
-                <button type="button" className="btn btn-secondary" data-dismiss="modal">{this.props.okBtnTitle}</button>
+                <button type="button" className="btn btn-secondary" data-dismiss="modal">{this.props.buttonTitle}</button>
             </div>
         );
     }
