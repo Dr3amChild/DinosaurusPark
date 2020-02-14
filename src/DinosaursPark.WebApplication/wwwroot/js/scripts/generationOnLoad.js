5e4c8120723ac1c4832139f888f0ce265203d589
Aleksandr Kochagov <kochagov@lecta.ru>
2020-02-14 11:27:05 +0300

    a dirty workaround for generation page

--- src/DinosaursPark.WebApplication/wwwroot/js/scripts/generationOnLoad.js
+++ src/DinosaursPark.WebApplication/wwwroot/js/scripts/generationOnLoad.js
@@ -3,6 +3,7 @@ const ReactDOM = window.ReactDOM;
 const redirect = window.Redirect;
 
 window.onload = async () => {
+    await new DinosaursApi(10).getPage(1);
     const generation = new Generation(10, 100, 10, redirect);
     generation.showForm();
 };
